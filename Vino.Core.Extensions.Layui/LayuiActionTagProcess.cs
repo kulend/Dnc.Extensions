f8b4ed07258af99652c1231393a0bdb509917a8c
kulend <kulend@qq.comn>
2018-03-22 17:55:12 +0800

    none

---
+++ Vino.Core.Extensions.Layui/LayuiActionTagProcess.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Vino.Core.Extensions.Ui;
+
+namespace Vino.Core.Extensions.Layui
+{
+    public class LayuiActionTagProcess : IActionTagProcess
+    {
+        protected readonly LayuiOption _layuiOption;
+
+        public LayuiActionTagProcess(IOptions<LayuiOption> layuiOption)
+        {
+            this._layuiOption = layuiOption.Value;
+        }
+
+        public async Task ProcessAsync(ActionTagHelper helper, TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button";
+            output.TagMode = TagMode.StartTagAndEndTag;
+            var cls = "layui-btn";
+
+            var hasThemeCss = !string.IsNullOrEmpty(helper.Css) && (helper.Css.Contains("layui-btn-primary") 
+                || helper.Css.Contains("layui-btn-normal") || helper.Css.Contains("layui-btn-warm") 
+                || helper.Css.Contains("layui-btn-danger") || helper.Css.Contains("layui-btn-disabled"));
+            var hasSizeCss = !string.IsNullOrEmpty(helper.Css) && (helper.Css.Contains("layui-btn-lg")
+                || helper.Css.Contains("layui-btn-sm") || helper.Css.Contains("layui-btn-xs"));
+
+            if (!string.IsNullOrEmpty(_layuiOption.ActionTagTheme) && !hasThemeCss)
+            {
+                cls += $" {_layuiOption.ActionTagTheme}";
+            }
+            if (!string.IsNullOrEmpty(_layuiOption.ActionTagSize) && !hasSizeCss)
+            {
+                cls += $" {_layuiOption.ActionTagSize}";
+            }
+
+            if (!string.IsNullOrEmpty(helper.Css))
+            {
+                cls += $" {helper.Css}";
+            }
+            output.Attributes.SetAttribute("class", cls);
+            output.Attributes.SetAttribute("type", "button");
+            output.Attributes.SetAttribute("title", helper.Title ?? helper.Text ?? "");
+
+            var content = "";
+            if (!string.IsNullOrEmpty(helper.Icon))
+            {
+                content = $"<i class=\"layui-icon\">{helper.Icon}</i>";
+            }
+            if (!string.IsNullOrEmpty(helper.Text))
+            {
+                content += helper.Text;
+            }
+            output.Content.SetHtmlContent(content);
+        }
+    }
+}
---
+++ Vino.Core.Extensions.Ui/ActionTagHelper.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vino.Core.Extensions.Ui
+{
+    public class ActionTagHelper : TagHelper
+    {
+        private IActionTagProcess _process;
+
+        public ActionTagHelper(IActionTagProcess process)
+        {
+            this._process = process;
+        }
+
+        [HtmlAttributeNotBound]
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public string Text { set; get; } = "";
+        public string Css { set; get; } = "";
+        public string Icon { set; get; }
+        public string Title { set; get; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            await _process.ProcessAsync(this, context, output);
+        }
+    }
+}
